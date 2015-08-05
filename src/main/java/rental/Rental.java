4fa5d4cbb13b65eff734e7ad3338f06dfe4fc742
David Rodrigues <david.rodrigues@inventsoftware.com.br>
2015-08-05 11:28:40 -0300

    Primeiro cenario para refatoração

---
+++ src/main/java/rental/Rental.java
@@ -0,0 +1,22 @@
+package rental;
+
+/**
+ * Created by David on 05/08/2015.
+ */
+public class Rental {
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+}
